22cda0c634a9dfb97c0b301ea36b9bb57fe9b42c
taesik mun <taesic12@knou.ac.kr>
2023-07-27 19:05:22 +0900

    Err variable , Comment CRUD

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,9 +2,17 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
 )
 
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("Not implemented")
+)
+
+// Store - this interface defines all of the methods
+// that our service needs in order to operate
 type Store interface {
 	GetComment(context.Context, string) (Comment, error)
 }
@@ -36,8 +44,18 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
