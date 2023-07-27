ab05da3ba616f15605b8b033bfb18d12e7ce7326
taesik mun <taesic12@knou.ac.kr>
2023-07-27 20:24:40 +0900

    sqlx, pq dependencies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"go-rest/internal/db"
+)
 
 func Run() error {
 	fmt.Println("starting up ")
+
+	database, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to create database")
+		return err
+	}
+	if err := database.Ping(context.Background()); err != nil {
+		return err
+	}
+
+	fmt.Println("Successfully connected and pinged database")
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go-rest
 
 go 1.20
+
+require github.com/jmoiron/sqlx v1.3.5
+
+require github.com/lib/pq v1.10.9 // indirect
